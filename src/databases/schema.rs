33fbe9a4a882993476299e9e0cf6adfb9bfe91ab
Tempestmon <misha8936@yandex.ru>
2024-03-05 12:28:04 +0500

    feat(db): create post table with relation

--- src/databases/schema.rs
+++ src/databases/schema.rs
@@ -1,8 +1,25 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    posts (id) {
+        id -> Integer,
+        creation_timestamp -> Timestamp,
+        title -> Text,
+        body -> Text,
+        user_id -> Integer,
+    }
+}
+
+diesel::table! {
     users (id) {
-        id -> Int4,
+        id -> Integer,
         name -> Text,
     }
 }
+
+diesel::joinable!(posts -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
--- src/databases/sqlite.rs
+++ src/databases/sqlite.rs
@@ -1,3 +1,4 @@
+use std::time::SystemTime;
 use diesel::{Connection, Queryable, Selectable, SqliteConnection};
 
 pub fn establish_connection() -> SqliteConnection {
@@ -11,4 +12,16 @@ pub fn establish_connection() -> SqliteConnection {
 pub(crate) struct User {
     id: i32,
     pub(crate) name: String,
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::databases::schema::posts)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+#[diesel(belongs_to(User, foreign_key = user_id))]
+struct Post {
+    id: i32,
+    creation_timestamp: SystemTime,
+    title: String,
+    body: String,
+    user_id: i32,
 }
